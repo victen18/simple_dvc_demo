6bf49878577f5631f46ae6e7b70f10c8d9eb494e
victen <vikramnimmakuri@gmail.com>
2022-03-24 02:55:25 -0700

    setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wineq pacakge",
+    author="victen",
+    packages=find_packages(),
+    license="MIT",
+    author_email="vikramnimmakuri@gmail.com"
+)
+
